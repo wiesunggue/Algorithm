815ae1f7919a7a3787fa3ad924aa20f45ca29142
wie0213 <wie0213@hanyang.ac.kr>
2023-12-12 14:02:47 +0900

    BOJ algorithm study

--- math/Sogang_muscle.py
+++
@@ -1,3 +0,0 @@
-# https://www.acmicpc.net/problem/20300
-# 백준 20300번 서강근육맨 문제
-
--- math/energy_drink.py
+++
@@ -1,9 +0,0 @@
-# https://www.acmicpc.net/problem/20115
-# 백준 20115번 에너지 드링크 문제
-import sys
-
-N = int(input())
-arr = list(map(int,sys.stdin.readline().split()))
-
-M = max(arr)
-print(M/2+sum(arr)/2)
