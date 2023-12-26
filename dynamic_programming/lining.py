7ed84547f3ef9c987183b821c5bdedc5520645fa
wie0213 <wie0213@hanyang.ac.kr>
2023-12-26 13:44:07 +0900

    BOJ algorithm study

--- dynamic_programming/lining.py
+++ dynamic_programming/lining.py
@@ -1,3 +1,17 @@
 # https://www.acmicpc.net/problem/2631
 # 백준 2631번 줄세우기 문제
 
+N = int(input())
+arr = [int(input()) for i in range(N)]
+dp = [0]*N
+print(arr)
+def lis():
+    for i in range(N):
+        dp[i] = 1
+        for j in range(i):
+            if arr[j]<arr[i]:
+                dp[i] = max(dp[i],dp[j]+1)
+lis()
+print(dp)
+print(N-max(dp))
+
---
+++ math/max_min.py
@@ -0,0 +1,12 @@
+# https://www.acmicpc.net/problem/30961
+# 백준 30961번 최솟값, 최댓값 문제
+
+N = int(input())
+arr = sorted(map(int,input().split()))
+ans = 0
+for i in range(N-1):
+    ans ^= arr[i]*arr[i+1]
+for i in range(N):
+    ans ^= arr[i]*arr[i]
+
+print(ans)
