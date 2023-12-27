168c600528cd0953cdac71876392d2bffe5f0372
wie0213 <wie0213@hanyang.ac.kr>
2023-12-27 23:25:43 +0900

    BOJ algorithm study

--- dynamic_programming/lotto.py
+++ dynamic_programming/lotto.py
@@ -7,8 +7,20 @@ def solution():
     if 2**(N-1)>M:
         return 0
 
-    dp = [[0]*N for i in range(M)] #dp[N][M]
-    dp
+    dp = [[0]*(M+1) for i in range(N+1)] #dp[N][M]
+    for i in range(M+1):
+        dp[1][i] = 1
+    for i in range(2,N+1):
+        for j in range(1,M+1):
+#            for k in range(j//2+1):
+#                print(i,j,k)
+#                dp[i][j] += dp[i-1][k]
+            if j%2==0:
+                dp[i][j] = dp[i][j-1] + dp[i-1][j//2]
+            else:
+                dp[i][j] = dp[i][j-1]
+    print(*dp,sep='\n')
+    return sum(dp[N])
 
 T = int(input())
 
