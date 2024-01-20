c9b11037ae69a88f5406b7c931ad8ede5034e1a1
wie0213 <wie0213@hanyang.ac.kr>
2024-01-20 14:04:16 +0900

    BOJ algorithm study

--- two_pointer/conveyer_sushi.py
+++ two_pointer/conveyer_sushi.py
@@ -1,5 +1,29 @@
 # https://www.acmicpc.net/problem/15961
 # 백준 15961번 회전초밥 문제
 
-N,d,K,c = map(int,input().split())
+import sys
+input = sys.stdin.readline
+
+N,d,k,c = map(int,input().split())
 arr = [int(input()) for i in range(N)]
+state_table = [0]*(d+1)
+state_table[c] = -10*10
+idx = k
+cases = 0
+ans = 0
+for i in range(k):
+    state_table[arr[i%N]]+=1
+    cases += (state_table[arr[i%N]] == 1)
+ans = cases
+print(ans)
+while idx<=N+k:
+    print(state_table)
+    before1, before2 = state_table[arr[idx%N]], state_table[arr[(idx-k)%N]]
+    state_table[arr[idx%N]] += 1
+    state_table[arr[(idx-k)%N]] -= 1
+    cases += (before1==0 and state_table[arr[idx%N]]==1)-(before2==1 and state_table[arr[(idx-k)%N]]==0)
+    print(cases)
+    ans = max(ans,cases)
+    idx += 1
+
+print(ans+1)
