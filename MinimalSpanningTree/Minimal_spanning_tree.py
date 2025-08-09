4f0da6b8bb0ea5ec2b237ecc896018586d7a0ba9
wie0213 <wie0213@hanyang.ac.kr>
2025-08-09 17:24:59 +0900

    BOJ algorithm study

--- MinimalSpanningTree/Minimal_spanning_tree.py
+++ MinimalSpanningTree/Minimal_spanning_tree.py
@@ -7,3 +7,35 @@ class UnionFind:
         self.N = N
         self.Parent = [i for i in range(N)]
         self.Rank = [1 for i in range(N)]
+
+    def Find(self,u):
+        if self.Parent[u] == u:
+            return u
+        return self.Find(self.Parent[u])
+
+    def merge(self,u,v):
+        u,v = self.Find(u), self.Find(v)
+
+        if u == v:
+            return False
+
+        if self.Rank[u] < self.Rank[v]:
+            u, v = v, u
+        self.Parent[v] = u
+        if self.Rank[u] == self.Rank[v]:
+            self.Rank[u] += 1
+
+        return True
+
+V, E = map(int,input().split())
+
+edges = [list(map(int,input().split())) for i in range(E)]
+
+edges.sort(key = lambda x: x[2])
+ans = 0
+union = UnionFind(V)
+for i in range(E):
+    if union.merge(edges[i][0],edges[i][1]):
+        ans += edges[i][2]
+
+print(ans)
